8e7be52ab98103b2d951e35d4d244fae397319a1
Henry Addo <addhen@linuxmail.org>
2015-11-13 13:40:31 +0900

    Add a reactive location manager for android

---
+++ library/raiburari/src/main/java/com/addhen/android/raiburari/presentation/location/LastKnownLocationObservable.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 Henry Addo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.addhen.android.raiburari.presentation.location;
+
+import android.content.Context;
+import android.location.Location;
+
+import rx.Observable;
+import rx.Observer;
+
+/**
+ * Fetches the users known last location
+ */
+public class LastKnownLocationObservable extends BaseLocationObservable<Location> {
+
+    protected LastKnownLocationObservable(Context context) {
+        super(context);
+    }
+
+    public static Observable<Location> createObservable(Context ctx) {
+        return Observable.create(new LastKnownLocationObservable(ctx));
+    }
+
+    @Override
+    protected void onLocationFixed(Observer<? super Location> observer) {
+        Location location = getLasKnowLocation(observer);
+        if (location != null) {
+            observer.onNext(location);
+        }
+        observer.onCompleted();
+    }
+}
---
+++ library/raiburari/src/main/java/com/addhen/android/raiburari/presentation/location/LocationUpdatesObservable.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2015 Henry Addo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.addhen.android.raiburari.presentation.location;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import rx.Observable;
+import rx.Observer;
+
+public class LocationUpdatesObservable extends BaseLocationObservable<Location> {
+
+    protected LocationUpdatesObservable(Context context) {
+        super(context);
+    }
+
+    public static Observable<Location> createObservable(Context context) {
+        return Observable.create(new LocationUpdatesObservable(context));
+    }
+
+    @Override
+    protected void onLocationFixed(final Observer<? super Location> observer) {
+        setLocationListener(new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location != null) {
+                    observer.onNext(location);
+                }
+                stopLocating();
+                observer.onCompleted();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
+        getLocationUpdates(observer);
+    }
+}
