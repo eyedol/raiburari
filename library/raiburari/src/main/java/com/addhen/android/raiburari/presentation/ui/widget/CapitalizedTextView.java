2247ed6b15c83d07843bb9d657c2d36e9e8e2a13
Henry Addo <addhen@linuxmail.org>
2016-05-11 16:20:39 +0900

    Call toUpperCase with the default locale as reported by lint

--- library/raiburari/src/main/java/com/addhen/android/raiburari/presentation/ui/widget/CapitalizedTextView.java
+++ library/raiburari/src/main/java/com/addhen/android/raiburari/presentation/ui/widget/CapitalizedTextView.java
@@ -20,6 +20,8 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 /**
  * Custom view to capitalize the first letter of a sentence. Sadly Android doest support this out
  * of the box.
@@ -42,7 +44,7 @@ public class CapitalizedTextView extends TextView {
     public void setText(CharSequence txt, BufferType type) {
         CharSequence text = txt;
         if (text.length() > 0) {
-            text = String.valueOf(text.charAt(0)).toUpperCase() + text
+            text = String.valueOf(text.charAt(0)).toUpperCase(Locale.getDefault()) + text
                     .subSequence(1, text.length());
         }
         super.setText(text, type);
